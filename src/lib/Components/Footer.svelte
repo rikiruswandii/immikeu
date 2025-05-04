9dd325655e6d9fb93404cd7c62505b8c08951f94
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-05-04 21:24:38 +0700

    style: text

--- src/lib/Components/Footer.svelte
+++ src/lib/Components/Footer.svelte
@@ -3,6 +3,6 @@
 	const siteName = PUBLIC_SITE_NAME || 'Mikeu';
 </script>
 
-<footer id="contact" class="border-t-2 border-solid border-surface-light/55 text-center py-6 text-surface-light text-shadow-surface-light/55 font-semibold">
+<footer id="contact" class="border-t-2 border-solid border-surface-light/55 text-center py-6 text-gray-300">
 	<p>Copyright &copy; 2025 {siteName}.</p>
 </footer>
